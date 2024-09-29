f9e727c5b09e37e4768329bfdca03d677b09ee9a
Patrick Franco <patrickxfranco@gmail.com>
2024-09-29 13:47:20 -0300

    Add files via upload

---
+++ src/components/Header/styles.js
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
+
+export const Container = styled.header`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin: 0 24px;
+	padding: 16px 0;
+
+	border-bottom-width: 1px;
+	border-bottom-style: solid;
+	border-bottom-color: ${({ theme }) => theme.COLORS.GRAY_500};
+
+	> h1 {
+		font-size: 2rem;
+	}
+`;
+
+export const Menu = styled.button`
+	background-color: transparent;
+	border: none;
+	display: none;
+
+	> svg {
+		font-size: 2.5rem;
+		color: ${({ theme }) => theme.COLORS.BLUE_200};
+	}
+
+	@media (max-width: ${DEVICE_BREAKPOINTS.MD}) {
+    display: block;
+	}
+`;
